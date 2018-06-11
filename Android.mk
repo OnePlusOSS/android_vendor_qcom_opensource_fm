5804cba0e51099ead24f0f7f9bcc8ec35b8f6594
AnubhavGupta <anubhavg@codeaurora.org>
2018-06-11 13:06:41 +0530

    FM: add changes for FTM
     - create a seperate library for FTM
     - create library in vendor image
    
    Change-Id: I87b01c787cfe9245614f805d92f14d25ab37f331

--- Android.mk
+++ Android.mk
@@ -15,3 +15,8 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 LOCAL_INIT_RC := init.qti.fm.rc
 
 include $(BUILD_PREBUILT)
+
+LOCAL_SHARED_LIBRARIES := liblog
+
+LOCAL_PATH := $(LOCAL_DIR_PATH)
+include $(LOCAL_PATH)/fm_hci/Android.mk
